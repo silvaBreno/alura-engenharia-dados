0d38ecdfe46bf1fe51b4f7cb4eb9a3e9f414fca8
F1685239 Breno De Souza Silva <breno.souza@bb.com.br>
2025-07-30 08:41:50 -0300

    criando meu primeiro DAG

--- pipeline-apache-airflow/airflowalura/dags/meu_primeiro_dag.py
+++ pipeline-apache-airflow/airflowalura/dags/meu_primeiro_dag.py
@@ -3,12 +3,19 @@ from airflow.utils.dates import days_ago
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.bash_operator import BashOperator
 
-@Class
-@SELECT
-class DAG(
+with DAG(
     dag_id='meu_primeiro_dag',
     start_date=days_ago(1),
     schedule_interval='@daily'
 ) as dag:
 
-    tarefa_1 = EmptyOperator(task_id= 'tarefa_1')
+    tarefa_1 = EmptyOperator(task_id = 'tarefa_1')
+    tarefa_2 = EmptyOperator(task_id = 'tarefa_2')
+    tarefa_3 = EmptyOperator(task_id = 'tarefa_3')
+    tarefa_4 = BashOperator(
+        task_id = 'cria_pasta',
+        bash_command = 'mkdir -p "../../pasta"'
+    )
+
+    tarefa_1 >> [tarefa_2, tarefa_3] >> tarefa_4
+    tarefa_3 >> tarefa_4
